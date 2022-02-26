65156d92a7a82b9501e794607904726d7ba4181f
Notnik_kg <dinararahatbekkyzy@gmaiil.com>
2022-02-26 16:44:35 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        Кундорго enum тузунуз (Monday, Tuesday ...)
+//Бир кунду белгилениз консолдон. Эгер Monday болсо - анда
+// "Дуйшомбу куну жава окуйм" же Tuesday болсо - анда
+// "Шейшемби куну англис тили сабагын окуйм" деп кунго жараша кайсы сабакты окуганынызды консольго чыгарыныз.
+//        Жуманын кундорун кыргыз тилинде корсотунуз.
+
+        Days sheishembi = Days.SHEISHEMBI;
+        System.out.println(sheishembi);
+        sheishembi.method();
+
+
+
+
+
+
+
+
+
+    }
+}
