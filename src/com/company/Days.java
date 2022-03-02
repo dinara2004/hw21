ebdf1d53f0da6691a125cdfb72273c2a63e4a639
Notnik_kg <dinararahatbekkyzy@gmaiil.com>
2022-03-02 14:28:24 +0600

    hw21

--- src/com/company/Days.java
+++ src/com/company/Days.java
@@ -1,51 +1,16 @@
 package com.company;
 
 public enum Days {
-    DUISHOMBU{
-        @Override
-        public void method() {
-            System.out.println("Биринчи куну теория болот.");
-        }
-    },
-    SHEISHEMBI{
-        @Override
-        public void method() {
-            System.out.println("Экинчи куну практика + софт скиллс болот.");
-        }
-    },
-    SHARSHEMBI{
-        @Override
-        public void method() {
-            System.out.println("Учунчу куну теория болот.");
-        }
-    },
-    BEISHEMBI{
-        @Override
-        public void method() {
-            System.out.println("Тортунчу куну практика + английский болот.");
-        }
-    },
-    JUMA{
-        @Override
-        public void method() {
-            System.out.println("Бешинчи куну ивент + теория болот.");
-        }
-    },
-    ISHEMBI{
-        @Override
-        public void method() {
-            System.out.println("Алтынчы куну Чынгыз агайдын сешны болот.");
-        }
-    },
-    JEKSHEMBI{
-        @Override
-        public void method() {
-            System.out.println("Жетинчи куну эс алуу.");
-        }
-    };
+    DUISHOMBU,
+    SHEISHEMBI,
+    SHARSHEMBI,
+    BEISHEMBI,
+    JUMA,
+    ISHEMBI,
+    JEKSHEMBI;
 
 
-    public abstract void method();
+
 
 
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,27 +1,47 @@
 package com.company;
 
+import java.util.Locale;
 import java.util.Scanner;
+
+import static com.company.Days.DUISHOMBU;
 
 public class Main {
 
     public static void main(String[] args) {
-//        Кундорго enum тузунуз (Monday, Tuesday ...)
-//Бир кунду белгилениз консолдон. Эгер Monday болсо - анда
-// "Дуйшомбу куну жава окуйм" же Tuesday болсо - анда
-// "Шейшемби куну англис тили сабагын окуйм" деп кунго жараша кайсы сабакты окуганынызды консольго чыгарыныз.
-//        Жуманын кундорун кыргыз тилинде корсотунуз.
-
-        Days sheishembi = Days.SHEISHEMBI;
-        System.out.println(sheishembi);
-        sheishembi.method();
-
-
-
-
-
-
-
-
-
+        Days days = null;
+    System.out.println("Day: ");
+    Scanner input = new Scanner(System.in);
+    String nameOfDay = input.nextLine();
+        try {
+            days = Days.valueOf(String.valueOf(nameOfDay.toUpperCase(Locale.ROOT)));
+        } catch (IllegalArgumentException e) {
+            System.err.println("НЕКОРРЕКТНАЯ ДАТА!");
+        }
+       if (days != null){
+        switch (days) {
+            case DUISHOMBU:
+                System.out.println("Биринчи куну теория болот");
+                break;
+            case SHEISHEMBI:
+                System.out.println("Экинчи куну практика + софт скиллс болот");
+                break;
+            case SHARSHEMBI:
+                System.out.println("Учунчу куну теория болот.");
+                break;
+            case BEISHEMBI:
+                System.out.println("Тортунчу куну практика + английский болот.");
+                break;
+            case JUMA:
+                System.out.println("Бешинчи куну ивент + теория болот.");
+                break;
+            case ISHEMBI:
+                System.out.println("Алтынчы куну Чынгыз агайдын сешны болот.");
+                break;
+            case JEKSHEMBI:
+                System.out.println("Жетинчи куну эс алуу.");
+                break;
+        }
+           System.out.println(days);
+        }
     }
 }
